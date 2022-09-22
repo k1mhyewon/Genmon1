d9a14c4e33e3c609bae8fc95997d74fcc7630eb4
jyeost <pig9619@naver.com>
2022-09-22 15:36:19 +0900

    초기화 및 공동으로 필요한 파일들 커밋함

---
+++ Genmon1/src/main/java/common/util/security/SecretMyKey.java
@@ -0,0 +1,7 @@
+package common.util.security;
+
+public class SecretMyKey {
+	
+	// 암호화 키 길면 길수록 암호화 알고리즘이 복잡하기 때문에 뚫리기 어려움 // 대신 복호화 오래걸림
+	public final static String KEY = "abcd0070#aclass$"; 
+}
---
+++ Genmon1/src/main/webapp/WEB-INF/jsonview.jsp
@@ -0,0 +1,4 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%-- request 영역에 저장된 문자열 json을 가져온 것임 {"isExists":false} --%>
+${requestScope.json}
