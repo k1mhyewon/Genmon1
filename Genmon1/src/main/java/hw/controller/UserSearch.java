00c36c190de5211f3b9f00838a373bf329719575
k1mhyewon <kimhy0434@gmail.com>
2022-09-24 17:23:03 +0900

    혜원 리뷰 수정 커밋

---
+++ Genmon1/src/main/java/hw/controller/UserSearch.java
@@ -0,0 +1,24 @@
+package hw.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.controller.AbstractController;
+
+public class UserSearch extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		try {
+			// super.setRedirect(false);
+			super.setViewPage("/WEB-INF/hw/userSearch.jsp");
+			} catch(Exception e) {
+				e.printStackTrace();
+				super.setRedirect(true);
+				super.setViewPage(request.getContextPath()+"/error.sun");
+			}
+		
+	}
+
+}
