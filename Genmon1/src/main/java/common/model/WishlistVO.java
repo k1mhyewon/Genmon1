3243a0b14d91972e81796ed725de2b59abcc0208
k1mhyewon <kimhy0434@gmail.com>
2022-09-29 17:48:24 +0900

    abs 컨트롤러 주석 해제

--- Genmon1/src/main/java/common/model/WishlistVO.java
+++ Genmon1/src/main/java/common/model/WishlistVO.java
@@ -4,12 +4,21 @@ public class WishlistVO {
 	
 	private String fk_userid;         // 회원아이디 - tbl_member 의 userid 
 	private String fk_pnum;           // 개별상품코드 - tbl_all_product 의 pnum
+
+	
+	
+	
+	// ===================================================================================== //
+	
+	
 	public String getFk_userid() {
 		return fk_userid;
 	}
 	public void setFk_userid(String fk_userid) {
 		this.fk_userid = fk_userid;
 	}
+	
+	
 	public String getFk_pnum() {
 		return fk_pnum;
 	}
@@ -17,9 +26,5 @@ public class WishlistVO {
 		this.fk_pnum = fk_pnum;
 	}
 	
-	
-	
-	
-	// ===================================================================================== //
 
 }
