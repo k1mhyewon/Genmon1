b0fc8dcb36731feadc235430da1abd8c370ab63c
k1mhyewon <kimhy0434@gmail.com>
2022-09-29 22:42:17 +0900

    위시리스트 select 완

--- Genmon1/src/main/java/hw/model/WishlistVO.java
+++ Genmon1/src/main/java/hw/model/WishlistVO.java
@@ -5,10 +5,16 @@ public class WishlistVO {
 	private String fk_userid;         // 회원아이디 - tbl_member 의 userid 
 	private String fk_pnum;           // 개별상품코드 - tbl_all_product 의 pnum
 
-	
-	
+	/*
+	  *********** 나중에 ProductVO 생기면 바꾸기***********
+	private ProductVO prodvo;
+	*/
+	private String pname;
+	private String price;
+	private String pimage1;
 	
 	// ===================================================================================== //
+	
 	
 	
 	public String getFk_userid() {
@@ -25,6 +31,33 @@ public class WishlistVO {
 	public void setFk_pnum(String fk_pnum) {
 		this.fk_pnum = fk_pnum;
 	}
+	
+	
+	
+	// ------------------- 나중에 삭제 ------------------ //
+	
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getPimage1() {
+		return pimage1;
+	}
+	public void setPimage1(String pimage1) {
+		this.pimage1 = pimage1;
+	}
+	
+	
+	// ----------------------------------------------- //
+	
 	
 
 }
