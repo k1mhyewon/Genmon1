09bc5a175a4b2604013308aeb9f462ece823514e
jyeost <pig9619@naver.com>
2022-10-03 04:40:58 +0900

    자자이제...

--- Genmon1/src/main/java/hw/controller/CartToPurchase.java
+++ Genmon1/src/main/java/hw/controller/CartToPurchase.java
@@ -21,11 +21,9 @@ public class CartToPurchase extends AbstractController {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
+		String method = request.getMethod();
 		// post 방식으로 넘어오긴 했는데 나눠줘야 하나??
-		
-		if(super.checkLogin(request)) { // 로그인 한 상태라면 바로 주문으로 가면 안대고 ㅎㅎ
-	        
-			
+		if("post".equalsIgnoreCase(method)) {
 			String all_qty = request.getParameter("all_qty");
 			String all_pnum = request.getParameter("all_pnum");
 			
@@ -48,31 +46,39 @@ public class CartToPurchase extends AbstractController {
 			session.setAttribute("ordertList", ordertList);
 			// request.setAttribute("ordertList", ordertList);
 			
-			// --------------------------------------------------------------- //
 			
-			
-	         // HttpSession session =  request.getSession(); 
-	           // 메모리에 생성되어져 있는 session 을 불러오는 것이다.
-	         MemberVO loginuser = (MemberVO)session.getAttribute("loginuser");
-	         
-	         request.setAttribute("email", loginuser.getEmail());
-	         request.setAttribute("name", loginuser.getName());
-	         request.setAttribute("mobile", loginuser.getMobile());
-	         request.setAttribute("postcode", loginuser.getPostcode());
-	         request.setAttribute("address", loginuser.getAddress());
-	         request.setAttribute("detailAddress", loginuser.getDetailaddress());
-	         request.setAttribute("extraAddress", loginuser.getExtraaddress());
-	         
-	         
-	         // --------------------------------------------------------------- //
-	         
-	         super.setRedirect(false);
-	         super.setViewPage("/WEB-INF/jihyun/deliveryInfo.jsp");
-	         
-	      } else { // 로그인 안 한 상태라면 로그인 체크로
-	         super.setRedirect(false);
-	         super.setViewPage("/WEB-INF/jihyun/checkLogin.jsp");
-	      }
+			if(super.checkLogin(request)) { // 로그인 한 상태라면 바로 주문으로 가면 안대고 ㅎㅎ
+		        
+				// --------------------------------------------------------------- //
+				
+				
+		         // HttpSession session =  request.getSession(); 
+		           // 메모리에 생성되어져 있는 session 을 불러오는 것이다.
+		         MemberVO loginuser = (MemberVO)session.getAttribute("loginuser");
+		         
+		         request.setAttribute("email", loginuser.getEmail());
+		         request.setAttribute("name", loginuser.getName());
+		         request.setAttribute("mobile", loginuser.getMobile());
+		         request.setAttribute("postcode", loginuser.getPostcode());
+		         request.setAttribute("address", loginuser.getAddress());
+		         request.setAttribute("detailAddress", loginuser.getDetailaddress());
+		         request.setAttribute("extraAddress", loginuser.getExtraaddress());
+		         
+		         
+		         // --------------------------------------------------------------- //
+		         
+		         super.setRedirect(false);
+		         super.setViewPage("/WEB-INF/jihyun/deliveryInfo.jsp");
+		         
+		      } else { // 로그인 안 한 상태라면 로그인 체크로
+		    	  
+		    	  
+		         super.setRedirect(false);
+		         super.setViewPage("/WEB-INF/jihyun/checkLogin.jsp");
+		      }
+		}
+		
+		
 		
 	}
 
