e75839b546aad41510d9d70fa9b3f27c2617cafe
k1mhyewon <kimhy0434@gmail.com>
2022-10-03 21:57:10 +0900

    비밀번호 찾기 끝

--- Genmon1/src/main/java/hw/controller/PwdChangeEnd.java
+++ Genmon1/src/main/java/hw/controller/PwdChangeEnd.java
@@ -1,4 +1,4 @@
-package hw.controller;
+ package hw.controller;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,10 +17,11 @@ public class PwdChangeEnd extends AbstractController {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
 		// System.out.println("===== PwdUpdateEnd 로 넘어옴 =====");
+		HttpSession session = request.getSession();
 		
-		String userid = request.getParameter("userid"); // 인증이 성공했을때만 들어옴
+		String userid = (String) session.getAttribute("userid"); // 인증이 성공했을때만 들어옴
 		
-		// System.out.println("~~~~~~~~~~~~~~~~ userid : "+userid);
+		System.out.println("~~~~~~~~~~~~~~~~ userid : "+userid);
 		
 		String method = request.getMethod();
 		
@@ -33,36 +34,57 @@ public class PwdChangeEnd extends AbstractController {
 			paraMap.put("pwd", pwd);
 			paraMap.put("userid", userid);
 			
+			System.out.println("pwd : "+pwd);
+			System.out.println("userid : "+userid);
+			
 			InterMemberDAO mdao = new MemberDAO();
-			int n = mdao.pwdUpdate(paraMap);
 			
-			request.setAttribute("n", n);
+			boolean isExistPwd = mdao.isExistPwd(paraMap);
+			System.out.println("isExistPwd : "+isExistPwd);
 			
-			String message = "";
-			String loc = "";
+			request.setAttribute("isExistPwd", isExistPwd);
 			
-			HttpSession session = request.getSession();
-			
-			if(n == 1) {
-				message = "비밀번호가 변경되었습니다. 다시 로그인해주세요.";
+			if(isExistPwd) {
 				
+				// System.out.println("이미 사용중인 비밀번호 / 세션에 저장 / isExistPwd => "+isExistPwd);
+				super.setRedirect(false);
+				super.setViewPage("/WEB-INF/hw/pwdChange.jsp");
 				
 			}
 			else {
-				message = "오류가 발생했습니다. 다시 시도해주세요.";
 				
+				int n = mdao.pwdUpdate(paraMap);
+				
+				request.setAttribute("n", n);
+				
+				String message = "";
+				String loc = "";
+				
+				
+				if(n == 1) {
+					message = "비밀번호가 변경되었습니다. 새로운 비밀번호로 로그인해주세요.";
+					
+					
+				}
+				else {
+					message = "오류가 발생했습니다. 다시 시도해주세요.";
+					
+				}
+				
+				// !!!! 세션에 저장된 인증코드 삭제하기 !!!! 
+				session.removeAttribute("certificationCode");
+				session.removeAttribute("userid");
+				
+				request.setAttribute("message", message);
+				
+				super.setRedirect(false);
+				super.setViewPage("/WEB-INF/hw/pwdMsg.jsp");
 			}
 			
-			// !!!! 세션에 저장된 인증코드 삭제하기 !!!! 
-			session.removeAttribute("certificationCode");
-			
-			request.setAttribute("message", message);
 			
 			
-			// super.setRedirect(false);
-			super.setViewPage("/WEB-INF/hw/pwdMsg.jsp");
 			
-		} 
+		} // end of if("POST".equalsIgnoreCase(method)) {} -------------------------------------- 
 		
 	}
 
