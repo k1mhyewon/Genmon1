1a11fd6b25efe2d43f5a13749d71c18ef6ae0620
k1mhyewon <kimhy0434@gmail.com>
2022-10-10 14:40:43 +0900

    어드민 사이드바 리뷰 연결

---
+++ Genmon1/src/main/java/hw/controller/ManageReview.java
@@ -0,0 +1,16 @@
+package hw.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.controller.AbstractController;
+
+public class ManageReview extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		
+	}
+
+}
